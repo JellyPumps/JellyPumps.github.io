6fa339ab13e5f7009e7f70858cd7ee8c59516904
Jelly <88274478+JellyPumps@users.noreply.github.com>
2022-10-03 13:25:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//navbarCol on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarCol');
+    }
+    else {
+        header.classList.remove('navbarCol');
+    }
+}
